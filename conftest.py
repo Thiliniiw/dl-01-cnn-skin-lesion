f105d5b9a09b85dff19c4e82c748b8d45dd65ec5
Thilini <amthiliniiw@gmail.com>
2026-04-20 13:27:11 -0400

    feat: add project config, utils, pytest setup and utils tests

---
+++ conftest.py
@@ -0,0 +1,5 @@
+"""
+Pytest configuration file.
+Placing this at the project root tells pytest to add the root
+to sys.path, making `src` importable from anywhere in the project.
+"""
---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+dl-01-cnn-skin-lesion
+---------------------
+Skin lesion classification using ResNet-50 and Grad-CAM.
+Dataset: ISIC 2019
+"""
